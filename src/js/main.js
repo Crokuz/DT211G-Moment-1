faca4daa7e6c4e2cf5d9fdbcef00b81d6aebab74
Crokuz <leander.norberg@hotmail.com>
2026-01-26 23:41:18 +0100

    Added JavaScript functinality to navigation menu

--- src/js/main.js
+++ src/js/main.js
@@ -1,1 +1,25 @@
-console.log("waddup");
+/*
+Författare: Leander Norberg
+Projketnamn: Moment 1
+Beskrivning: Automatisering & publicering, 
+Frontend-baserad webbutveckling (DT211G) VT26
+*/
+"use strict";
+
+let openMenu = document.getElementById("openButton");
+let closeMenu = document.getElementById("closeButton");
+
+openMenu.addEventListener('click', toggleMenu);
+closeMenu.addEventListener('click', toggleMenu);
+
+//Funktion för att öppna och stänga menyn 
+function toggleMenu() {
+    let navMenu = document.getElementById("navMenu");
+    let style = window.getComputedStyle(navMenu);
+
+    if(style.display === "none") {
+        navMenu.style.display = "block";
+    } else {
+        navMenu.style.display = "none";
+    }
+}
